c14f70d98520ff5b4de0b64495b12bb6561711a0
martin90909090 <personalmperez@gmail.com>
2023-12-20 20:47:56 -0300

    m

--- assets/JS/main2.js
+++ assets/JS/main2.js
@@ -1,16 +1,16 @@
-let cantidad_1 = document.getElementById("cantidad_1").textContent;
-let cantidad_2 = document.getElementById("cantidad_2").textContent;
-let cantidad_3 = document.getElementById("cantidad_3").textContent;
-let paragraph = document.getElementById("paragraph").textContent;
+let cantidad_1 = document.getElementById("cantidad_1");
+let cantidad_2 = document.getElementById("cantidad_2");
+let cantidad_3 = document.getElementById("cantidad_3");
+let paragraph = document.getElementById("paragraph");
 let btn = document.getElementById("btn");
 cantidad_1 = 0, cantidad_2 = 0, cantidad_3 = 0;
-let c_total = cantidad_1 + cantidad_2 + cantidad_3;
 
 btn.addEventListener("click", () => {
-    if(c_total <= 10){
+    let c_total = cantidad_1 + cantidad_2 + cantidad_3;
+    if(c_total >= 0 && c_total <= 10){
         paragraph.textContent = "Van " + c_total + " stickers"; 
     }
-    else{
+    else if(c_total > 10){
         paragraph.textContent = "Llevas demasiados stickers";
     }
 });
