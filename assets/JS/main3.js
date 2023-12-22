a3d51c4e43492990ec9e80fcf0d9aa58c9e38847
martin90909090 <personalmperez@gmail.com>
2023-12-22 20:45:41 -0300

    m

--- assets/JS/main3.js
+++ assets/JS/main3.js
@@ -1,18 +1,16 @@
 const password_1 = 911;
 const password_2 = 714;
-let btn = document.getElementById("button");
-let message = document.getElementById("p");
+let btn = document.getElementById("btn");
+let message = document.getElementById("paragraph");
 
 btn.addEventListener('click', () => {
     let digito_1 = document.getElementById("digito_1").value;
     let digito_2 = document.getElementById("digito_2").value;
     let digito_3 = document.getElementById("digito_3").value;
-    let password_i = parseInt(digito_1) + parseInt(digito_2) + parseInt(digito_3);
-    if(password_1 == password_i){
+    let password_i = parseInt(digito_1 + digito_2 + digito_3);
+
+    if(password_1 === password_i || password_2 === password_i){
         message.textContent = "Password 1 correcto";
-    }
-    else if(password_2 == password_i){
-        message.textContent = "Password 2 correcto";
     }
     else{
         message.textContent = "Password incorrecto";
