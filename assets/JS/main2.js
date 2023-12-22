f810606dfd0adb7e192ff91f9479b817e06ddb5b
martin90909090 <personalmperez@gmail.com>
2023-12-22 17:24:05 -0300

    m

--- assets/JS/main2.js
+++ assets/JS/main2.js
@@ -1,10 +1,10 @@
-let cantidad_1 = document.getElementById("cantidad_1");
-let cantidad_2 = document.getElementById("cantidad_2");
-let cantidad_3 = document.getElementById("cantidad_3");
 let paragraph = document.getElementById("paragraph");
 let btn = document.getElementById("btn");
 
 btn.addEventListener("click", () => {
+    let cantidad_1 = document.getElementById("cantidad_1").value;
+    let cantidad_2 = document.getElementById("cantidad_2").value;
+    let cantidad_3 = document.getElementById("cantidad_3").value;
     let c_total = parseInt(cantidad_1) + parseInt(cantidad_2) + parseInt(cantidad_3);
     if(c_total >= 0 && c_total <= 10){
         paragraph.textContent = "Van " + c_total + " stickers"; 
--- assets/JS/main3.js
+++ assets/JS/main3.js
@@ -1,20 +1,20 @@
 const password_1 = 911;
 const password_2 = 714;
-let digito_1 = document.querySelector("digito_1").textContent;
-let digito_2 = document.querySelector("digito_2").textContent;
-let digito_3 = document.querySelector("digito_3").textContent;
-let btn = document.querySelector("button").textContent;
-let message = document.querySelector("p").textContent;
-digito_1, digito_2, digito_3 = 0;
+let btn = document.getElementById("button");
+let message = document.getElementById("p");
+
 btn.addEventListener('click', () => {
-    let password_i = digito_1 + digito_2 +digito_3;
+    let digito_1 = document.getElementById("digito_1").value;
+    let digito_2 = document.getElementById("digito_2").value;
+    let digito_3 = document.getElementById("digito_3").value;
+    let password_i = parseInt(digito_1) + parseInt(digito_2) + parseInt(digito_3);
     if(password_1 == password_i){
-        message = "Password 1 correcto"
+        message.textContent = "Password 1 correcto";
     }
-    if(password_2 == password_i){
-        message = "Password 2 correcto"
+    else if(password_2 == password_i){
+        message.textContent = "Password 2 correcto";
     }
     else{
-        message = "Password incorrecto"
+        message.textContent = "Password incorrecto";
     }
 });
