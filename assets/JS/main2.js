84762738180f42235ccc99c81c81241dd50f08b9
martin90909090 <personalmperez@gmail.com>
2023-12-22 16:49:24 -0300

    m

--- assets/JS/main2.js
+++ assets/JS/main2.js
@@ -3,10 +3,9 @@ let cantidad_2 = document.getElementById("cantidad_2");
 let cantidad_3 = document.getElementById("cantidad_3");
 let paragraph = document.getElementById("paragraph");
 let btn = document.getElementById("btn");
-cantidad_1 = 0, cantidad_2 = 0, cantidad_3 = 0;
 
 btn.addEventListener("click", () => {
-    let c_total = cantidad_1 + cantidad_2 + cantidad_3;
+    let c_total = parseInt(cantidad_1) + parseInt(cantidad_2) + parseInt(cantidad_3);
     if(c_total >= 0 && c_total <= 10){
         paragraph.textContent = "Van " + c_total + " stickers"; 
     }
